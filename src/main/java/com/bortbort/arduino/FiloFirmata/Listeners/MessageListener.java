3d0ab50c5c98e90850f3f0e8a4bb67a60d715d92
reapzor <reapzor@gmail.com>
2016-01-07 01:38:44 -0500

    The generics made no difference, remove them

--- src/main/java/com/bortbort/arduino/FiloFirmata/Listeners/MessageListener.java
+++ src/main/java/com/bortbort/arduino/FiloFirmata/Listeners/MessageListener.java
@@ -6,7 +6,7 @@ import com.bortbort.arduino.FiloFirmata.Messages.Message;
  * Created by chuck on 1/6/2016.
  */
 public abstract class MessageListener<T extends Message> {
-    private Class<? extends MessageListener> messageListenerClass;
+    private Class messageListenerClass;
 
     public MessageListener(Class<? extends MessageListener> messageListenerClass) {
         this.messageListenerClass = messageListenerClass;
@@ -14,7 +14,7 @@ public abstract class MessageListener<T extends Message> {
 
     public abstract void messageReceived(T message);
 
-    public Class<? extends MessageListener> getMessageListenerClass() {
+    public Class getMessageListenerClass() {
         return messageListenerClass;
     }
 }
--- src/main/java/com/bortbort/arduino/FiloFirmata/MessageDispatcher.java
+++ src/main/java/com/bortbort/arduino/FiloFirmata/MessageDispatcher.java
@@ -10,14 +10,13 @@ import java.util.HashMap;
  * Created by chuck on 1/6/2016.
  */
 public class MessageDispatcher {
-    private final HashMap<Class<? extends MessageListener>,
-            ArrayList<MessageListener>> messageListenerMap = new HashMap<>();
+    private final HashMap<Class, ArrayList<MessageListener>> messageListenerMap = new HashMap<>();
 
     public MessageDispatcher() {
     }
 
-    public void addMessageListener(MessageListener<? extends Message> messageListener) {
-        Class<? extends MessageListener> messageListenerClass = messageListener.getMessageListenerClass();
+    public void addMessageListener(MessageListener messageListener) {
+        Class messageListenerClass = messageListener.getMessageListenerClass();
 
         if (!messageListenerMap.containsKey(messageListenerClass)) {
             ArrayList<MessageListener> listenerArray = new ArrayList<>();
@@ -43,9 +42,10 @@ public class MessageDispatcher {
     //   implementations, which we are also generic here.
     @SuppressWarnings("unchecked")
     public void dispatchMessage(Message message) {
-        Class<? extends MessageListener> messageListenerClass = message.getMessageListenerClass();
+        Class messageListenerClass = message.getMessageListenerClass();
         if (messageListenerMap.containsKey(messageListenerClass)) {
-            ArrayList<MessageListener> messageListeners = messageListenerMap.get(messageListenerClass);
+            ArrayList<MessageListener> messageListeners =
+                    messageListenerMap.get(messageListenerClass);
             for (MessageListener listener : messageListeners) {
                 listener.messageReceived(message);
             }
